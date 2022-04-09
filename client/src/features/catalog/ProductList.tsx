cd9e24c3968b9adbb27eb621ed8283800fe42300
Kenneth Forseth Camilleri <kenneth.forseth@gmail.com>
2022-04-09 21:34:40 +0200

    Added Paging Sorting and Filtering

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -2,19 +2,26 @@ import { Grid } from "@mui/material";
 import { Product } from "../../app/models/product";
 import React from 'react';
 import ProductCard from "./ProductCard";
+import ProductCardSkeleton from "./ProductCardSkeleton";
+import { useAppSelector } from "../../app/store/configureStore";
 
 interface Props {
     products: Product[];
 }
 
 export default function ProductList({products}: Props) {
+    const {productsLoaded} = useAppSelector(state => state.catalog)
     return (
         <Grid container spacing={4}>
             {products.map(product => (
-                <Grid key={product.id} item xs={3}>
-                    <ProductCard product={product} />
+                <Grid key={product.id} item xs={4}>
+
+                    {!productsLoaded ? (
+                        <ProductCardSkeleton />
+                    ) : (
+                        <ProductCard product={product} />
+                    )}
                 </Grid>
-                
             ))}
         </Grid>
     )
